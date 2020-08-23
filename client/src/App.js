77dfba8d22afeb025c5ff5436e2f8474bb81d526
Amneh-Hammami <hammamiamneh7@gmail.com>
2020-08-23 20:37:48 +0300

    Creating react js project for client & Adding React Router

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {BrowserRouter,Route} from 'react-router-dom'
+import "./App.css"
+
+import NavBar from './components/Navbar'
+
+import Home from './components/screens/Home'
+import Login from './components/screens/Login'
+import Signup from './components/screens/Signup'
+import Profile from './components/screens/Profile'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+
+     <NavBar />
+     <Route  exact path="/">
+       <Home />
+     </Route>
+     <Route path="/login">
+       <Login />
+     </Route>
+     <Route path="/signup">
+       <Signup/>
+     </Route>
+     <Route path="/profile">
+       <Profile />
+     </Route>
+
+    </BrowserRouter>
+  
+
+  );
+}
+
+export default App;
