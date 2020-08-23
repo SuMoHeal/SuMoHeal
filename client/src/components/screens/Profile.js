1d1168070b989d169fbe0e36e24eeb54f2fac18a
Amneh-Hammami <hammamiamneh7@gmail.com>
2020-08-23 22:26:30 +0300

    Creating Profile page UI in react

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React from 'react'
 
-const Profile = ()=>{
-   
+
+const Profile = () =>{
     return(
-      <div className="home">
-        
-       <h1>Profile</h1>
+        <div style={{maxWidth:"550px", margin:"0px auto"}}>
+            <div style={{
+                display:"flex",
+                justifyContent:"space-around",
+                margin:"18px 0px",
+                borderBottom:"1px solid grey"
+            }}>
+                <div>
+                    <img style={{width:"160px",height:"160px",borderRadius:"80px"}}
+                     src="https://img.favpng.com/23/4/11/computer-icons-user-profile-avatar-png-favpng-QsYtjsW73M0aGLb4GbMEyLbc5.jpg"
+                    />
+                </div>
+                <div>
+                <h4>Amneh Hammami</h4>
+                <div style={{display:"flex",justifyContent:"space-between",width:"108%"}}>
+                    <h6>40 posts</h6>
+                    <h6>40 followers</h6>
+                    <h6>40 following</h6>
+                </div>
+                </div>
+            </div>
+          <div className="gallery">
+           <img className="item" src="https://img.favpng.com/23/4/11/computer-icons-user-profile-avatar-png-favpng-QsYtjsW73M0aGLb4GbMEyLbc5.jpg "/>
+           <img className="item" src="https://img.favpng.com/23/4/11/computer-icons-user-profile-avatar-png-favpng-QsYtjsW73M0aGLb4GbMEyLbc5.jpg "/>
+           <img className="item" src="https://img.favpng.com/23/4/11/computer-icons-user-profile-avatar-png-favpng-QsYtjsW73M0aGLb4GbMEyLbc5.jpg "/>
+           <img className="item" src="https://img.favpng.com/23/4/11/computer-icons-user-profile-avatar-png-favpng-QsYtjsW73M0aGLb4GbMEyLbc5.jpg "/>
+           <img className="item" src="https://img.favpng.com/23/4/11/computer-icons-user-profile-avatar-png-favpng-QsYtjsW73M0aGLb4GbMEyLbc5.jpg "/>
+           <img className="item" src="https://img.favpng.com/23/4/11/computer-icons-user-profile-avatar-png-favpng-QsYtjsW73M0aGLb4GbMEyLbc5.jpg "/>
+           <img className="item" src="https://img.favpng.com/23/4/11/computer-icons-user-profile-avatar-png-favpng-QsYtjsW73M0aGLb4GbMEyLbc5.jpg "/>
+
           </div>
-        
+        </div>
     )
 }
 
