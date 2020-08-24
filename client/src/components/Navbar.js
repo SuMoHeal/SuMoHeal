66585e6c57fec437bd99d0351d9a76e40c98189f
Amneh-Hammami <hammamiamneh7@gmail.com>
2020-08-24 22:26:23 +0300

    Changing Navigation links on logingin

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,18 +1,28 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import {Link} from 'react-router-dom'
+import {UserContext} from '../App'
 
 const NavBar = () =>{
-    return(
-       
+  const {state,dispatch} = useContext(UserContext)
+  const renderList = ()=>{
+    if(state){
+     return [
+      <li><Link to="/profile">Profile</Link></li>,
+      <li><Link to="/create">Create Post</Link></li>
+     ]
+    }else{
+      return [
+        <li><Link to="/login">Login</Link></li>,
+        <li><Link to="/signup">Signup</Link></li>
+      ]
+    }
+  }
+  return( 
       <nav>
   <div className="nav-wrapper #0d47a1 blue darken-4" >
-          <Link to="/" className="brand-logo left">SuMoHeale</Link> 
+          <Link to={state?"/":"/login"} className="brand-logo left">SuMoHeale</Link> 
           <ul id="nav-mobile" className="right">
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
-            <li><Link to="/profile">Profile</Link></li>
-            <li><Link to="/create">Create Post</Link></li>
-
+           {renderList()}
           </ul>
         </div>
   </nav>
