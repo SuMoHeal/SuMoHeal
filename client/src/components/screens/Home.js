b59def39fab66ea1a921704c93621394c2199d34
Amneh-Hammami <hammamiamneh7@gmail.com>
2020-08-24 10:50:42 +0300

    Creating Home page UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -4,10 +4,35 @@ const Home = ()=>{
    
     return(
       <div className="home">
-        
-       <h1>video about us </h1>
-          </div>
-        
+      <div className="card home-card">
+<div className="card-image waves-effect waves-block waves-light">
+  <img className="activator" src="https://image.shutterstock.com/image-illustration/3d-illustration-flower-background3d-wallpaper-260nw-1392043274.jpg" />
+</div>
+<div className="card-content">
+  <span className="card-title activator grey-text text-darken-4"> Title
+  <i className="material-icons right">more_vert</i>
+      <i className="material-icons right"> delete_forever </i>
+      <i className="material-icons right">  healing </i>
+  </span>
+  <span className="card-title activator grey-text text-darken-4">Amount</span>
+  <div>
+  <span>Hospital Name</span>
+  </div>
+  <div>
+  <span>Hospital Adress</span>
+  </div>
+  <div>
+  <span>Hospital phoneNumber</span>
+  </div>
+  <div>
+  <span>Patient phoneNumber</span>
+  </div>
+  <div>
+  <span>Description About Patient Health & Situation</span>
+  </div>
+</div> 
+</div>
+</div>
     )
 }
 
